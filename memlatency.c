35ce215602c348a09504aadb37d45ef8f554784f
Jason Koh <bk7749@gmail.com>
2015-02-17 03:11:34 +0000

    Almost there. Stay on target.

--- memlatency.c
+++ memlatency.c
@@ -10,9 +10,9 @@
 #include <sys/stat.h>
 #include <math.h>
 
-//const int LATENCY_ACCESS_TRIALS = 100000000;
+const int LATENCY_ACCESS_TRIALS = 100000000;
 //const int LATENCY_ACCESS_TRIALS = 1000000;
-const int LATENCY_ACCESS_TRIALS = 1000;
+//const int LATENCY_ACCESS_TRIALS = 1000;
 size_t INT_SIZE = sizeof(int);
 //256B
 const size_t array_size0 = 256;
@@ -54,7 +54,6 @@ void make_cyclic_array(void *, size_t);
 data_t memory_latency(data_t ccnt_overhead)
 {
 	memory_latency_helper(ccnt_overhead, array_size0);
-	/*
 	memory_latency_helper(ccnt_overhead, array_size1);
 	memory_latency_helper(ccnt_overhead, array_size2);
 	memory_latency_helper(ccnt_overhead, array_size3);
@@ -70,7 +69,6 @@ data_t memory_latency(data_t ccnt_overhead)
 	memory_latency_helper(ccnt_overhead, array_size13);
 	memory_latency_helper(ccnt_overhead, array_size14);
 	memory_latency_helper(ccnt_overhead, array_size15);
-	*/
 	return 0;
 }
 
@@ -99,7 +97,7 @@ data_t memory_latency_helper(data_t ccnt_overhead, size_t size)
 	avg = total / LATENCY_ACCESS_TRIALS;
 	printf("Size: %u\t Average Access Time: %f\n",
 			size, avg);
-	printf("Dummy: %d\n", *cur);
+	//printf("Dummy: %d\n", *cur);
 	free(array);
 	return 0;
 }
@@ -107,15 +105,16 @@ data_t memory_latency_helper(data_t ccnt_overhead, size_t size)
 void make_cyclic_array(void * array, size_t size)
 {
 	void ** ptrarray =  (void **) array;
-	for (unsigned int i=0; i<size; i++)
+	unsigned int arraysize = size / sizeof(void *);
+	for (unsigned int i=0; i<arraysize; i++)
 	{
 		ptrarray[i] = &ptrarray[i];
 	}
 
 	srand(0);
-	for (unsigned int i=size-1; i>0; i--)
+	for (unsigned int i=arraysize-1; i>0; i--)
 	{
-		unsigned int swp = rand() % size;
+		unsigned int swp = rand() % arraysize;
 		void * tmp = ptrarray[i];
 		ptrarray[i] = ptrarray[swp];
 		ptrarray[swp] = tmp;
