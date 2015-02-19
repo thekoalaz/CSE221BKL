a0c152dbcbcea38284956bc35eff012834ca00f6
Jason Koh <bk7749@gmail.com>
2015-02-19 10:17:55 +0000

    Milestone 2.

--- memlatency.c
+++ memlatency.c
@@ -5,11 +5,13 @@
 #include <math.h>
 #include <memory.h>
 
-//static const int LATENCY_ACCESS_TRIALS = 1000000000;
-const int LATENCY_ACCESS_TRIALS = 100000000;
-//const int LATENCY_ACCESS_TRIALS = 1000000;
-//const int LATENCY_ACCESS_TRIALS = 1000;
+//static const unsigned int LATENCY_ACCESS_TRIALS = 1000000000;
+static const unsigned int LATENCY_ACCESS_TRIALS = 100000000;
+//static const unsigned int LATENCY_ACCESS_TRIALS = 1000000;
+//static const unsigned int LATENCY_ACCESS_TRIALS = 1000;
 static const size_t INT_SIZE = sizeof(int);
+static const unsigned int STRIDE_TRIALS = 10;
+static const unsigned int STRIDE_START = 128;
 //256B
 static const size_t array_size0 = 256;
 //512B
@@ -50,41 +52,46 @@ static const size_t array_size17 = 256*pow(2,17);
 static const size_t array_size18 = 256*pow(2,18);
 
 
-data_t memory_latency_helper(data_t, size_t);
-void make_cyclic_array(void *, size_t);
+data_t memory_latency_helper(data_t, size_t, unsigned int);
+void make_cyclic_array(int *, size_t, unsigned int);
 unsigned int uniform_rand(unsigned int);
 
 data_t memory_latency(data_t ccnt_overhead)
 {
-	memory_latency_helper(ccnt_overhead, array_size0);
-	memory_latency_helper(ccnt_overhead, array_size1);
-	memory_latency_helper(ccnt_overhead, array_size2);
-	memory_latency_helper(ccnt_overhead, array_size3);
-	memory_latency_helper(ccnt_overhead, array_size4);
-	memory_latency_helper(ccnt_overhead, array_size5);
-	memory_latency_helper(ccnt_overhead, array_size6);
-	memory_latency_helper(ccnt_overhead, array_size7);
-	memory_latency_helper(ccnt_overhead, array_size8);
-	memory_latency_helper(ccnt_overhead, array_size9);
-	memory_latency_helper(ccnt_overhead, array_size10);
-	memory_latency_helper(ccnt_overhead, array_size11);
-	memory_latency_helper(ccnt_overhead, array_size12);
-	memory_latency_helper(ccnt_overhead, array_size13);
-	memory_latency_helper(ccnt_overhead, array_size14);
-	memory_latency_helper(ccnt_overhead, array_size15);
-	memory_latency_helper(ccnt_overhead, array_size16);
-	memory_latency_helper(ccnt_overhead, array_size17);
-	memory_latency_helper(ccnt_overhead, array_size18);
+	for(unsigned int i=0; i<STRIDE_TRIALS; i++)
+	{
+		unsigned int stride = STRIDE_START * pow(2,i) + 1;
+		printf("Trial #%u, Stride size: %u\n", i, stride);
+		memory_latency_helper(ccnt_overhead, array_size0, stride);
+		memory_latency_helper(ccnt_overhead, array_size1, stride);
+		memory_latency_helper(ccnt_overhead, array_size2, stride);
+		memory_latency_helper(ccnt_overhead, array_size3, stride);
+		memory_latency_helper(ccnt_overhead, array_size4, stride);
+		memory_latency_helper(ccnt_overhead, array_size5, stride);
+		memory_latency_helper(ccnt_overhead, array_size6, stride);
+		memory_latency_helper(ccnt_overhead, array_size7, stride);
+		memory_latency_helper(ccnt_overhead, array_size8, stride);
+		memory_latency_helper(ccnt_overhead, array_size9, stride);
+		memory_latency_helper(ccnt_overhead, array_size10, stride);
+		memory_latency_helper(ccnt_overhead, array_size11, stride);
+		memory_latency_helper(ccnt_overhead, array_size12, stride);
+		memory_latency_helper(ccnt_overhead, array_size13, stride);
+		memory_latency_helper(ccnt_overhead, array_size14, stride);
+		memory_latency_helper(ccnt_overhead, array_size15, stride);
+		memory_latency_helper(ccnt_overhead, array_size16, stride);
+		memory_latency_helper(ccnt_overhead, array_size17, stride);
+		memory_latency_helper(ccnt_overhead, array_size18, stride);
+	}
 
 	return 0;
 }
 
-data_t memory_latency_helper(data_t ccnt_overhead, size_t size)
+data_t memory_latency_helper(data_t ccnt_overhead, size_t size, unsigned int stride)
 {
 	unsigned start, end;
 	float avg=0;
 	float max = 0; 
-    float min = 50.0;
+    float min = 10000.0;
 	float squares_total = 0;
 	float stddev;
 
@@ -95,7 +102,7 @@ data_t memory_latency_helper(data_t ccnt_overhead, size_t size)
 		exit(1);
 	}
 	memset(array, 1, size);
-	make_cyclic_array(array, size);
+	make_cyclic_array(array, size, stride);
 
 	int * cur = array;
 	for(unsigned int i=0; i<LATENCY_ACCESS_TRIALS; i++)
@@ -118,22 +125,14 @@ data_t memory_latency_helper(data_t ccnt_overhead, size_t size)
 	return 0;
 }
 
-void make_cyclic_array(void * array, size_t size)
+void make_cyclic_array(int * array, size_t size, unsigned int stride)
 {
 	void ** ptrarray =  (void **) array;
-	unsigned int arraysize = size / sizeof(void *);
+	unsigned int arraysize = size / sizeof(int *);
+	unsigned int stridesize = stride / sizeof(int *);
 	for (unsigned int i=0; i<arraysize; i++)
 	{
-		ptrarray[i] = &ptrarray[i];
-	}
-
-	srand(time(NULL));
-	for (unsigned int i=arraysize-1; i>0; i--)
-	{
-		unsigned int swp = uniform_rand(arraysize);
-		void * tmp = ptrarray[i];
-		ptrarray[i] = ptrarray[swp];
-		ptrarray[swp] = tmp;
+		ptrarray[i] = &ptrarray[(i+stridesize)%arraysize];
 	}
 }
 
