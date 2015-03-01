89065981ce770d6946ab98d9de8bfabd0fbef5c6
Jason Koh <bk7749@gmail.com>
2015-03-01 00:37:34 +0000

    Added the separate main files.

---
+++ mem_main.c
@@ -0,0 +1,18 @@
+#include "mem_test.h"
+
+int main()
+{
+	data_t memlatency_result;
+
+	pmcr_init();
+
+	//Measurement overhead
+	data_t ccnt_overhead;
+	ccnt_overhead = get_overhead();
+	printf("Reading overhead time is : %f\n", ccnt_overhead);
+	
+	memlatency_result = memory_latency(ccnt_overhead);
+
+	return 0;
+}
+
